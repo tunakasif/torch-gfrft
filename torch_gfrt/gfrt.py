4d1201e921a4c498b88a186a781fe2a3a9ea335a
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2023-11-27 17:47:58 +0300

    feat(ndim): implementation of multi-dim gfrt
    Support GFRT at given dimension.

--- torch_gfrt/gfrt.py
+++ torch_gfrt/gfrt.py
@@ -12,9 +12,30 @@ class GFRT:
             self._eigvals, self._eigvecs = th.linalg.eig(gft_mtx)
             self._inv_eigvecs = th.linalg.inv(self._eigvecs)
 
-    def gfrt_mtx(self, a: float) -> th.Tensor:
+    def gfrt_mtx(self, a: float | th.Tensor) -> th.Tensor:
         fractional_eigvals = self._eigvals**a
         return th.einsum("ij,j,jk->ik", self._eigvecs, fractional_eigvals, self._inv_eigvecs)
 
-    def igfrt_mtx(self, a: float) -> th.Tensor:
+    def igfrt_mtx(self, a: float | th.Tensor) -> th.Tensor:
         return self.gfrt_mtx(-a)
+
+    def gfrt(self, x: th.Tensor, a: float | th.Tensor, dim: int = -1) -> th.Tensor:
+        gfrt_mtx = self.gfrt_mtx(a)
+        dtype = th.promote_types(gfrt_mtx.dtype, x.dtype)
+        return th.einsum(
+            get_einsum_str(len(x.shape), dim),
+            gfrt_mtx.type(dtype),
+            x.type(dtype),
+        )
+
+    def igfrt(self, x: th.Tensor, a: float | th.Tensor) -> th.Tensor:
+        return th.einsum("ij,j->i", self.igfrt_mtx(a), x)
+
+
+def get_einsum_str(dim_count: int, req_dim: int) -> str:
+    if req_dim < -dim_count or req_dim >= dim_count:
+        raise ValueError("Dimension size error.")
+    dim = th.remainder(req_dim, th.tensor(dim_count))
+    diff = dim_count - dim
+    remaining_str = "".join([chr(num) for num in range(98, 98 + diff)])
+    return f"ab,...{remaining_str}->...{remaining_str.replace('b', 'a', 1)}"
